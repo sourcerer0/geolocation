2591beb038d599c0b6b118573a48b1921f2ea1a1
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-18 16:53:31 -0300

    OS Classifier updated!

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: OS Independent",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     install_requires=[
