091490d2d07085a62108f7e585f5773bd4b5449c
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-22 22:08:04 -0300

    New update. More features. Search not implemented!

--- geolocation/tz/timezones.py
+++ geolocation/tz/timezones.py
@@ -2,17 +2,24 @@ from geopy.distance import great_circle
 
 class Timezones():
     def __init__(self, tz = "UTC"):
-        self.__timezone = tz
-
         self.__file = open("geolocation/tz/COORD_DB.txt", "r")
+
+        self.__ref_city = None
+        self.__timezone = tz
 
     def set_timezone(self, coordinates):
         if type(coordinates) != type(()):
             print("ERROR ****** Coordinates not accepted!******")
             return
 
+        ref_city = None
         for lines in self.__file.readlines():
-            pass
+            for line in self.__file.readlines(): line = line.split("\t")
+                ## DO SOME SEARCH HERE
+
+
+        self.__ref_city = (ref_city[1], ref_city[8], ref_city[4], ref_city[5])
+        self.__timezone = ref_city[17]
 
         return self.__timezone
 
@@ -21,4 +28,4 @@ class Timezones():
         return self.__timezone
 
     def __repr__(self):
-        return "Timezone: {}".format(self.__timezone)
+        return "Timezone: {}\n".format(self.__timezone)
