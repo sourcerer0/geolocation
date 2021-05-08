a952dbe04a3205bdd2310f5844577b9e556e82ef
elleaech <lohan.uchsa@protonmail.com>
2021-05-07 23:39:21 -0300

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="geolocation",
-    version="1.1.2",
+    version="2.0.0",
     author="Lo Han",
     author_email="lohan.uchsa@protonmail.com",
     description="Geolocation Library",
