1a58909487d6824dd87fc06d2c03420a642d0531
elleaech <lohan.uchsa@protonmail.com>
2021-05-08 14:01:23 -0300

    new tests

--- tests/test_timezones.py
+++ tests/test_timezones.py
@@ -5,7 +5,6 @@ from geolocation import Location
 class TestTimezones:
     somewhere = Timezones()
 
-    """
     def test_set_timezone(self):
         geo = Location("Rio de Janeiro, Rio de Janeiro, Brasil")
         assert self.somewhere.set_timezone(geo.coordinates) == {
@@ -13,4 +12,3 @@ class TestTimezones:
             "distance": 2.8266190065246817,
             "tz": "America/Sao_Paulo",
         }
-    """
