9b09c2d0f9aa9f50ace8c79a4e6398ca363181ba
elleaech <lohan.uchsa@protonmail.com>
2021-06-15 19:09:10 -0300

    'spot' and 'updated_view_coordinates' updated + new method to update map view zoom (auxiliary methods and attr added)

--- geolocation/map/map.py
+++ geolocation/map/map.py
@@ -6,25 +6,40 @@ import folium
 
 class Map:
     def __init__(self, coordinates: Coordinate, zoom: int = 20):
-        self._initial_coordinates = coordinates
-        self._map_view = folium.Map(
-            location=_convert_coordinates_to_list(self._initial_coordinates),
-            zoom_start=zoom,
-        )
+        self._current_coordinates = coordinates
+        self._map_init(zoom)
+
+        self._marked_places = list()
 
     def spot(
         self, coordinates: Coordinate, color: str, icon_type: str, inside_color: str
     ):
         marker = _Marker(color, icon_type, inside_color)
-        marker.mark_location(coordinates).add_to(self._map_view)
+        marked_location = marker.mark_location(coordinates)
 
-    def update_view_coordinates(self, coordinates: Coordinate, zoom: int = 20):
-        self._initial_coordinates = coordinates
-        self._map_view = folium.Map(
-            location=_convert_coordinates_to_list(coordinates),
-            zoom_start=zoom,
-        )
+        self._marked_places.append(marked_location)
+        marked_location.add_to(self._map_view)
+
+    def update_view_coordinates(self, coordinates: Coordinate):
+        self._current_coordinates = coordinates
+        self._map_view.location = _convert_coordinates_to_list(coordinates)
+
+    def update_map_zoom(self, zoom: int):
+        self._map_init(zoom)
+        self._mark_saved_locations()
 
     def render_html_view(self, output_file: str = "index.html"):
         self._map_view.render()
         return self._map_view.save(output_file)
+
+    def _mark_saved_locations(self):
+        for marked_place in self._marked_places:
+            print("here")
+            marked_place.add_to(self._map_view)
+
+    def _map_init(self, zoom: int):
+        self._map_view = folium.Map(
+            location=_convert_coordinates_to_list(self._current_coordinates),
+            zoom_start=zoom,
+            min_zoom=3,
+        )
